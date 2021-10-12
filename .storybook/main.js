f9fc9075df7c678e327bd889931ca64a490f3f08
Zyurtoglu <89520811+Zyurtoglu@users.noreply.github.com>
2021-10-12 12:10:26 +0300

    .

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,12 @@ const custom = require('../webpack.config.js');
 
 module.exports = {
   stories: ['../src/components/**/*.stories.[tj]s'],
-  addons: ['@storybook/addon-actions','@storybook/addon-a11y'],
+  addons: [
+      '@storybook/addon-actions',
+      '@storybook/addon-a11y',
+      '@storybook/addon-essentials',
+      "@storybook/preset-create-react-app"
+    ],
   webpackFinal: (config) => {
     return { ...config, module: { ...config.module, rules: custom.module.rules } };
   },
